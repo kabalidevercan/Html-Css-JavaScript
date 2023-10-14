791597778355d662fdd8a9ed0bcc8926cdf409b0
kabali ercan <kabalidevercan@gmail.com>
2023-10-14 19:35:13 +0300

    intersection

---
+++ Intersection_observer_ex/index.js
@@ -0,0 +1,52 @@
+
+const cardContainer = document.querySelector('.card:last-child')
+const cards = document.querySelectorAll('.card')
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show',entry.isIntersecting)
+         
+    })
+    
+},
+{
+    threshold: 0.9,
+    
+    
+}
+)
+
+
+
+
+
+
+const lastCardObserver = new IntersectionObserver(entries => {
+    const lastCard = entries[0]
+    if(!lastCard.isIntersecting) return
+    loadNewCards()
+    lastCardObserver.unobserve(lastCard.target)
+    lastCardObserver.observe(document.querySelector('.card:last-child'))
+
+}, {}
+
+)
+
+lastCardObserver.observe(document.querySelector('.card:last-child'))
+
+cards.forEach(card => {
+    observer.observe(card)
+})
+
+
+
+
+function loadNewCards () {
+    for(let i = 0; i < 10; i++) {
+        const card = document.createElement('div')
+        card.textContent = 'New Card'
+        card.classList.add('card')
+        observer.observe(card)
+        cardContainer.append(card)
+    }
+}
